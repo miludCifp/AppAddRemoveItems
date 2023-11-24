5bdce10bd14ba67a0951862ca8e04c3a2cb3a9b4
miludCifp <10395@cifpceuta.es>
2023-11-24 21:08:37 +0100

    Dar accion al boton Float del Dialog.

--- app/src/main/java/com/cifpceuta/appaddremoveitems/MainActivity.java
+++ app/src/main/java/com/cifpceuta/appaddremoveitems/MainActivity.java
@@ -79,45 +79,41 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                //Creamos el DIALOG asociado al ActivityMAIN
+                Dialog dialog = new Dialog(MainActivity.this);
+
+                //Le asociamos el layout correspondiente
+                dialog.setContentView(R.layout.dialogo_emergente);
+
+                //Recuperamos los views dentro de dicho layout para recuperar sus valores posteriormente
+                EditText etTextoItem = dialog.findViewById(R.id.etTextoItem);
+                Button btnDialogAñadir = dialog.findViewById(R.id.btnDialogAñadir);
+
+
+                //Establecemos el listener para capturar datos y realizar acción de añadir
+                btnDialogAñadir.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String item = etTextoItem.getText().toString();
+                        // tb se puede realizar una validación de campo no vacio...
+                        list_items.add(item);
+                        //añadimos el item al final
+                        adapter.notifyItemInserted(list_items.size()-1);
+
+                        //hacemos desplazar la lista de items hasta dicho valor
+                        rvListadoItems.scrollToPosition(list_items.size()-1);
+
+                        //Esta llamada cierra el dialogo
+                        dialog.dismiss();
+                    }
+                });
+
+                //Esta llamada abre el diálogo
+                dialog.show();
             }
         });
     }
-    public void onClick(View v) {
 
-        //Creamos el DIALOG asociado al ActivityMAIN
-        Dialog dialog = new Dialog(MainActivity.this);
-
-        //Le asociamos el layout correspondiente
-        dialog.setContentView(R.layout.dialogo_emergente);
-
-        //Recuperamos los views dentro de dicho layout para recuperar sus valores posteriormente
-        EditText etTextoItem = dialog.findViewById(R.id.etTextoItem);
-        Button btnDialogAñadir = dialog.findViewById(R.id.btnDialogAñadir);
-
-
-        //Establecemos el listener para capturar datos y realizar acción de añadir
-        btnDialogAñadir.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String item = etTextoItem.getText().toString();
-                // tb se puede realizar una validación de campo no vacio...
-                list_items.add(item);
-                //añadimos el item al final
-                adapter.notifyItemInserted(list_items.size()-1);
-
-                //hacemos desplazar la lista de items hasta dicho valor
-                rvListadoItems.scrollToPosition(list_items.size()-1);
-
-                //Esta llamada cierra el dialogo
-                dialog.dismiss();
-            }
-        });
-
-
-        //Esta llamada abre el diálogo
-        dialog.show();
-
-    }
     private void filtrado(String texto){
         ArrayList<String> filteredList_items = new ArrayList<>();
         for(String item : list_items){
